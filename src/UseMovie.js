17d27ca8907efea64e0475562aa2164e912dc355
Arman <125900320+Syfer0@users.noreply.github.com>
2024-02-07 20:34:48 +0530

    Catching rendering errors with an error boundary : error fix

--- src/UseMovie.js
+++ src/UseMovie.js
@@ -1,48 +1,50 @@
 import { useState, useEffect } from "react";
+
 const key = "341e3adc";
+
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
-  const [IsLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
-  useEffect(
-    function () {
-      //   callback?.();
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
-          const res = await fetch(
-            `https://www.omdbapi.com/?i=tt3896198&apikey=${key}&s=${query}`
-          );
-          if (!res.ok) {
-            throw new Error("Something went wrong");
-          }
-          const json = await res.json();
 
-          if (json.Response === "False") {
-            throw new Error("Movie Not Found");
-          }
+  useEffect(() => {
+    if (query.length <= 3) {
+      setMovies([]);
+      setError("");
+      return;
+    }
 
-          // const json = await res.json();
-          setMovies(json.Search);
-          // setTotalResults(json.totalResults);
-          console.log(json.Search);
-          setIsLoading(false);
-        } catch (err) {
-          console.log(err.message);
-          setError(err.message);
-        } finally {
-          setIsLoading(false);
-        }
-      }
-      if (query.length > 3) {
-        setMovies([]);
+    const fetchMovies = async () => {
+      try {
+        setIsLoading(true);
         setError("");
-      }
 
-      fetchMovies();
-    },
-    [query]
-  );
-  return { movies, IsLoading, error };
+        const res = await fetch(
+          `https://www.omdbapi.com/?i=tt3896198&apikey=${key}&s=${query}`
+        );
+
+        if (!res.ok) {
+          throw new Error("Something went wrong");
+        }
+
+        const json = await res.json();
+
+        if (json.Response === "False") {
+          throw new Error("Movie Not Found");
+        }
+
+        setMovies(json.Search);
+        console.log(json.Search);
+      } catch (err) {
+        console.error(err.message);
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, [query]);
+
+  return { movies, isLoading, error };
 }
